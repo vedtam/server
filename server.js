b11a4ebef35d6a9d809b539ea7812bc8a4be679f
vedtam <vedtam@yahoo.com>
2016-12-06 08:32:10 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+//import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import graphqlExpress from 'express-graphql';
 import cors from 'cors';
 import schema from './data/schema.js';
 import myResolver from './data/resolvers';
@@ -11,13 +12,12 @@ const graphQLServer = express().use('*', cors());
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({
   schema,
   context: {},
+  //formatError: (err) => { console.log(err.stack); return err },
 }));
 
-graphQLServer.use('/graphiql', graphiqlExpress({endpointURL: '/graphql',}));
-
-graphQLServer.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}));
+// graphQLServer.use('/graphiql', graphiqlExpress({
+//   endpointURL: '/graphql',
+// }));
 
 graphQLServer.listen(PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${PORT}/graphql`
